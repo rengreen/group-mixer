df99bc08937ee884ca7625d1065f80f6ef7312d6
rengreen <r.m.gardzinska@gmail.com>
2019-08-14 08:53:25 +0200

    Init project with Spring Initializr

---
+++ src/main/java/pl/rengreen/groupmixer/GroupMixerApplication.java
@@ -0,0 +1,13 @@
+package pl.rengreen.groupmixer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupMixerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupMixerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/rengreen/groupmixer/GroupMixerApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.rengreen.groupmixer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroupMixerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
